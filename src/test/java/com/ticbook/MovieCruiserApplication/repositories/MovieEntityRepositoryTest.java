2e5d371618e38bcb9e6cb3e97219994f81ada6da
Manobalan.A <manobalan.a@cognizant.com>
2018-08-28 11:36:00 +0530

    Update MovieEntityRepositoryTest.java

--- src/test/java/com/ticbook/MovieCruiserApplication/repositories/MovieEntityRepositoryTest.java
+++ src/test/java/com/ticbook/MovieCruiserApplication/repositories/MovieEntityRepositoryTest.java
@@ -20,7 +20,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.MovieCruiser.entities.MovieEntity;
+import com.ticbook.MovieCruiserApplication.entities.MovieEntity;
 
 import junit.framework.Assert;
 
