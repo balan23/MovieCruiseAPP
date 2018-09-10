760119f07e0d321cf0301e57415d10f5fdb9fe99
Manobalan.A <manobalan.a@cognizant.com>
2018-09-10 13:21:11 +0530

    Modified for Delete Functionality

--- src/main/java/com/ticbook/MovieCruiserApplication/controllers/MovieController.java
+++ src/main/java/com/ticbook/MovieCruiserApplication/controllers/MovieController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -83,7 +84,7 @@ public class MovieController {
 		return response;
 	}
 	
-	@PostMapping("/movies/delete/{movieId}")
+	@DeleteMapping("/movies/delete/{movieId}")
 	public ResponseEntity<?> deleteMovieDetails(@PathVariable("movieId") int movieId ){
 		ResponseEntity<?> response = null;
 		try {
