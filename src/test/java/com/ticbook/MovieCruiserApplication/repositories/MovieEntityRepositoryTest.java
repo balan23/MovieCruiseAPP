d21e8a0f9f15b99ef15e6468ad16004cad843006
Manobalan.A <manobalan.a@cognizant.com>
2018-09-17 14:59:16 +0530

    Modified Test Cased based on review comments

--- src/test/java/com/ticbook/MovieCruiserApplication/repositories/MovieEntityRepositoryTest.java
+++ src/test/java/com/ticbook/MovieCruiserApplication/repositories/MovieEntityRepositoryTest.java
@@ -4,13 +4,12 @@
  */
 package com.ticbook.MovieCruiserApplication.repositories;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,74 +17,104 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.ticbook.MovieCruiserApplication.entities.MovieEntity;
-
-import junit.framework.Assert;
 
 /**
  * @author Manobalan A
  *
  */
 @RunWith(SpringRunner.class)
-@Transactional
+//@Transactional
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class MovieEntityRepositoryTest{
 	
 	@Autowired
-	MovieEntityRepository movieEntityRepo;
+	private transient MovieEntityRepository movieEntityRepo;
+
 	
-	
-	MovieEntity movie = null;
-	MovieEntity movie2 = null;
-	List<MovieEntity> movieList = new ArrayList<>();
-	
-	@Before
-	public void populateMovie() {
-		movie = new MovieEntity();
-		movie.setMovieCode(1);
-		movie.setMovieName("The MEG 2018");
-		movie.setMovieComments("After escaping an attack by what he claims was a 70-foot shark, Jonas Taylor must confront his fears to save those trapped in a sunken submersible.");
-		movie.setPosterPath("https://www.imdb.com/title/tt4779682/mediaviewer/rm1110263296");
-		movieList.add(movie);
-		movie2 = new MovieEntity();
-		movie2.setMovieCode(2);
-		movie2.setMovieName("Hotel Transylvania 3: Summer Vacation (2018)");
-		movie2.setMovieComments("Count Dracula and company participate in a cruise for sea-loving monsters, unaware that their boat is being commandeered by the monster-hating Van Helsing family.");
-		movie2.setPosterPath("https://www.imdb.com/title/tt5220122/mediaviewer/rm1546076160");
-		movieList.add(movie2);
-		
-	}
+public void setRepo(final MovieEntityRepository movieEntityRepo) {
+	this.movieEntityRepo= movieEntityRepo;
+}
 	
 	@Test
-	public void saveMovieTest() {
-		assertEquals(1, movieEntityRepo.save(movie).getMovieCode());
+	public void saveMovieTest() throws Exception {
+		final MovieEntity mov = addMovie();
+		assertThat(mov.getMovieCode()).isEqualTo(1);
 	}
-	
-	@Test
-	public void findMovieByIdTest() {
+
+	/**
+	 * @return
+	 */
+	private MovieEntity addMovie() {
+		MovieEntity movie = new MovieEntity();
+		movie.setMovieCode(1);;
+		movie.setMovieName("seemaraja");
+		movie.setMovieComments("Siva's great movie");
+		movie.setPosterPath("www.glitz.com");
 		movieEntityRepo.save(movie);
-		Optional<MovieEntity> movie = movieEntityRepo.findById(1);
-		assertTrue(movie.isPresent());
-		assertEquals(1,movie.get().getMovieCode());
+		final MovieEntity mov=  movieEntityRepo.getOne(1);
+		return mov;
 	}
+	
+	@Test
+	public void updateMovieTest() throws Exception {
+
+		final MovieEntity uMov=  addMovie();
+		assertEquals(uMov.getMovieName(),"seemaraja");
+		uMov.setMovieComments("Rated 4.0");
+		movieEntityRepo.save(uMov);
+		final MovieEntity mov=  movieEntityRepo.getOne(1);
+		assertEquals(mov.getMovieComments(),"Rated 4.0");
+	}	
+
 	
 	@Test
 	public void deleteMovieByIdTest() {
-		movieEntityRepo.save(movie);
-		movieEntityRepo.deleteById(movie.getMovieCode());
-		Optional<MovieEntity> movie = movieEntityRepo.findById(1);
-		assertFalse(movie.isPresent());
+		
+		final MovieEntity mov=  addMovie();
+		assertEquals("seemaraja", mov.getMovieName());
+		movieEntityRepo.delete(mov);
+		assertEquals(Optional.empty(),movieEntityRepo.findById(1));
+
+	}
+	
+	@Test
+	public void findMovieByIdTest() {		
+		
+		final MovieEntity mov = addMovie();
+
+		assertEquals(1,mov.getMovieCode());
+	}
+	
+	@Test
+	public void findMovieByNameTest() {	
+		
+		final MovieEntity mov = addMovie();
+		assertEquals("seemaraja",mov.getMovieName());
 	}
 	
 	@Test
 	public void getAllMovieTest() {
+		MovieEntity movie= new MovieEntity();
+		movie.setMovieCode(1);;
+		movie.setMovieName("seemaraja");
+		movie.setMovieComments("Siva's great movie");
+		movie.setPosterPath("www.glitz.com");
 		movieEntityRepo.save(movie);
+		
+		MovieEntity movie2= new MovieEntity();
+		movie2.setMovieCode(2);;
+		movie2.setMovieName("alagu");
+		movie2.setMovieComments("Siva's great movie");
+		movie2.setPosterPath("www.glitz.com");
 		movieEntityRepo.save(movie2);
+
 		final List<MovieEntity> movieEntList= movieEntityRepo.findAll();
+		
 	assertEquals(movieEntList.get(0).getMovieCode(),1);
+	assertEquals(movieEntList.get(1).getMovieName(),"alagu");
 	}
 	
 
